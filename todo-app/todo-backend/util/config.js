07842a90793fddb085af03646c339745e87b6196
kariah <41475006+kariah@users.noreply.github.com>
2022-02-19 13:29:26 +0200

    Tehtävä 12.13

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,7 +1,9 @@
 const MONGO_URL = process.env.MONGO_URL || undefined
 const REDIS_URL = process.env.REDIS_URL || undefined
+const PORT = process.env.PORT || undefined
 
 module.exports = {
   MONGO_URL: 'mongodb://root:example@localhost:3456/database',
-  REDIS_URL: '//localhost:6379'
+  REDIS_URL: '//localhost:6379',
+  PORT: 3001
 }
